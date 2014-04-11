65f4826f4e1917a95179fe3be5a1b4fa25f43d9a
Yu Jiang Tham <ytham@apple.com>
2014-04-10 20:23:23 -0700

    Added index

--- server.js
+++ server.js
@@ -2,17 +2,50 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
-  app.use(express.logger('dev'));             // log every request to the console
-  app.use(express.bodyParser());              // pull information from html in POST
-  app.use(express.methodOverride());            // simulate DELETE and PUT
-});
+var secret = require('./secret');
+var mongoConnection = "mongodb://" + secret.user + ":" + secret.password + "@oceanic.mongohq.com:10093/gifro";
+
+mongoose.connect(mongoConnection);
+
+app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
+app.use(express.logger('dev'));             // log every request to the console
+app.use(express.bodyParser());              // pull information from html in POST
+app.use(express.methodOverride());            // simulate DELETE and PUT
+
 
 
 var Todo = mongoose.model('Todo', {
   text: String
 });
 
+app.get('/api/todos', function (req, res) {
+  Todo.find(function (err, todos) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function (req, res) {
+  Todo.create({
+    text: req.body.text,
+    done: false
+  }, function (err, todo) {
+    if (err) {
+      res.send(err);
+    }
+    Todo.find(function (err, todos) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(todos);
+    });
+  });
+});
+
+app.delete('/api/todos/:todo_id', function (req, res) {
+
+});
 
 app.listen(8888);
